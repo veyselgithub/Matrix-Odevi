6ab610ee420d929dc58d9b278241010e79dd7267
Veysel Doğan <doganveysel841@gmail.com>
2018-10-03 18:35:34 +0300

    Initial Commit

---
+++ Odev Deneme/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Odev Deneme
+//
+//  Created by Veysel on 3.10.2018.
+//  Copyright © 2018 Veysel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
