58f31e0c7b5953e0d0cc18c403dc2e4a33965f7a
veyselgithub <67361933+veyselgithub@users.noreply.github.com>
2020-11-04 20:15:02 +0300

    Matrix Odevi

--- Odev Deneme/main.cpp
+++ Odev Deneme/main.cpp
@@ -7,9 +7,37 @@
 //
 
 #include <iostream>
-
+using namespace std;
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int x,y;
+    cout<<"Satir degeri "<<endl;
+    cin>>x;
+    cout<<"Sutun degeri "<<endl;
+    cin>>y;
+    int matrix[x][y],i,j,enk,enb,sonuc;
+    
+    for(i=0;i<x;i++)
+        for(j=0;j<y;j++)
+        {
+            cout<<"["<<i<<","<<j<<"]";
+            cin>>matrix[i][j];
+        }
+    enk=matrix[0][0];
+    for(i=0;i<x;i++)
+        for(j=0;j<y;j++)
+        {
+            if(enk>matrix[i][j]) enk=matrix[i][j];
+        }
+    enb=matrix[0][0];
+    for(i=0;i<x;i++)
+        for(j=0;j<y;j++)
+        {
+            if(enb<matrix[i][j]) enb=matrix[i][j];
+        }
+    cout<<"En kucuk deger "<<enk<<endl;
+    cout<<"En buyuk deger "<<enb<<endl;
+    sonuc=(enb-enk);
+    cout<<"Fark "<<sonuc<<endl;
+    
     return 0;
 }
